7ce052439fc73fcde0cfeab05a2593e7b8e16338
Leonardo  Rodrigues <leonardo.rodrigues@ibm.com>
2021-09-29 03:56:50 -0300

    add dashboad ans first card

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
+
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon
+} from "./styles";
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/74029443?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Leonardo</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power"/>
+                </UserWrapper>
+            </Header>
+
+            <HighlightCard/>
+        </Container>
+    )
+}
+
+
