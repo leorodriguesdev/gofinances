7161a84b1755de89699bc9277f307d2000184033
Leonardo Rodrigues <leorodriguesoffice@gmail.com>
2022-02-22 16:19:37 -0300

    Error Google Authentication

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,8 +1,11 @@
 import React, { 
     createContext, 
     ReactNode,
-    useContext
+    useContext,
+//    useState
 } from 'react';
+
+import * as AuthSession from 'expo-auth-session';
 
 interface AuthProviderProps {
     children: ReactNode;
@@ -20,12 +23,26 @@ interface User {
 
 interface IAuthContextData {
     user: User;
+    signInWithGoogle(): Promise<void>;
 }
 
+interface AuthorizationResponse {
+    params: {
+    access_token: string;
+    };
+    type: string;
+}
 
 const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
+
+    {/* 
+    =============Comentado para debugar o erro de autenticação============
+    const [user, setUser] = useState<User>({} as User);
+    
+    */}
+
     const user = {
         id: '1',
         name: 'Fulano',
@@ -33,8 +50,45 @@ function AuthProvider({ children }: AuthProviderProps) {
         photo: 'https://avatars2.githubusercontent.com/u/63977?s=460&u=f9f8b8f8b8f8b8f8b8f8b8f8b8f8b8f8b8f8b8f8&v=4'
     };
 
+    async function signInWithGoogle(){
+        try {       
+            const CLIENT_ID = '513489943734-19evtfkti5reuq8bmbd3oh56d1e2pkot.apps.googleusercontent.com';
+            const REDIRECT_URI = 'https://auth.expo.io/@leorodriguesdev/gofinances';
+            const RESPONSE_TYPE = 'token';
+            const SCOPE = encodeURI('profile email');
+
+            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+            const { type, params } = await AuthSession
+            .startAsync({ authUrl }) as AuthorizationResponse;
+
+            if( type === 'success' ) {
+                const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&acess_token=${params.access_token}`);
+                const userInfo = await response.json();
+                console.log(userInfo);
+
+                {/* 
+
+                ======Comentado para debugar o erro de autenticação====
+                setUser({
+                    id: userInfo.id,
+                    name: userInfo.given_name,
+                    email: userInfo.email,
+                    photo: userInfo.picture,
+                });
+                
+                */}
+            }
+            } catch (error) {
+            throw new Error(error as string);
+            }
+    }
+
     return (
-        <AuthContext.Provider value={{ user }}>
+        <AuthContext.Provider value={{ 
+            user, 
+            signInWithGoogle
+            }}>
             {children}
         </AuthContext.Provider>
     );
--- src/screens/SighIn/index.tsx
+++ src/screens/SighIn/index.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React from "react";
+import { Alert } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import AppleSvg from '../../assets/apple.svg';
@@ -20,8 +21,20 @@ import {
 
 export function SighIn() {
 
-    const { user } = useAuth();
-//    console.log(user);
+    const { signInWithGoogle } = useAuth();
+
+    async function handleSignInWithGoogle() {
+        try {
+            await signInWithGoogle();
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Erro', 'Erro ao realizar o login');
+
+        }
+
+    }
+
+
 
     return(
         <Container>
@@ -49,6 +62,7 @@ export function SighIn() {
                     <SignInSocialButtom
                         title="Entrar com Google"
                         svg={GoogleSvg}
+                        onPress={handleSignInWithGoogle}
                     />
                     <SignInSocialButtom
                         title="Entrar com Apple"
